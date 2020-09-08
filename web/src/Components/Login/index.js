b759a4a783de88985324ba3813baa11d3d375b6e
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-09-08 16:25:36 -0300

    Adicionado Media Queries

--- web/src/Components/Login/index.js
+++ web/src/Components/Login/index.js
@@ -5,20 +5,23 @@ import LoginForm from './LoginForm'
 import LoginCreate from './LoginCreate'
 import LoginPerdeu from './LoginPerdeu'
 import LoginResetar from './LoginResetar'
+import styles from './Login.module.css'
 
 const Login = () => {
 	const { login } = React.useContext(UserContext)
 	if (login === true) return <Navigate to="/conta" />
 
 	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<LoginForm />} />
-				<Route path="criar" element={<LoginCreate />} />
-				<Route path="perdeu" element={<LoginPerdeu />} />
-				<Route path="resetar" element={<LoginResetar />} />
-			</Routes>
-		</div>
+		<section className={styles.login}>
+			<div className={styles.forms}>
+				<Routes>
+					<Route path="/" element={<LoginForm />} />
+					<Route path="criar" element={<LoginCreate />} />
+					<Route path="perdeu" element={<LoginPerdeu />} />
+					<Route path="resetar" element={<LoginResetar />} />
+				</Routes>
+			</div>
+		</section>
 	)
 }
 
