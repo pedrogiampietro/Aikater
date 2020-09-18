82c0d2ec336f8180a54ec314ed15aa421c6bc564
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-09-18 20:03:39 -0300

    Adicionado HeaderNav, Adicionado title dinamico

--- web/src/Components/User/index.js
+++ web/src/Components/User/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import UserHeader from './UserHeader'
+import Feed from '../Feed'
+import UserPhotoPost from './UserPhotoPost'
+import UserStats from './UserStats'
+import { Routes, Route } from 'react-router-dom'
 
 const User = () => {
 	return (
-		<div>
-			<h1>User</h1>
-		</div>
+		<section className="container">
+			<UserHeader />
+			<Routes>
+				<Route path="/" element={<Feed />} />
+				<Route path="estatisticas" element={<UserStats />} />
+				<Route path="postar" element={<UserPhotoPost />} />
+			</Routes>
+		</section>
 	)
 }
 
