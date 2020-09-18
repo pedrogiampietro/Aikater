f066369e871103935469520e52d933807f97247c
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-09-18 18:59:52 -0300

    Protected Router

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login';
-import { UserStorage } from './UserContext';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { UserStorage } from './UserContext'
+import ProtectedRoute from './Components/Helper/ProtectedRouter'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import User from './Components/User'
+
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-          </Routes>
-          <Footer />
-        </UserStorage>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<UserStorage>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="login/*" element={<Login />} />
+						<ProtectedRoute path="conta/*" element={<User />} />
+					</Routes>
+					<Footer />
+				</UserStorage>
+			</BrowserRouter>
+		</div>
+	)
 }
 
-export default App;
+export default App
